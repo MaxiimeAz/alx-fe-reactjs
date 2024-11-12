4e8cf96dac5e469c9a4b14a0c1cde5f70cd09168
MaxiimeAz <graffiti2015@gmail.com>
2024-11-12 00:46:13 +0000

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -7,3 +7,15 @@ function MainContent() {
 }
 
 export default MainContent;
+// MainContent.jsx
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
+      <p>Welcome to the page where you can explore my favorite cities!</p>
+    </main>
+  );
+}
+
+export default MainContent;
