b0f2fc6841fc7a27154c299766f2eb2987838239
MaxiimeAz <graffiti2015@gmail.com>
2024-11-12 00:46:35 +0000

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -7,3 +7,15 @@ function Footer() {
 }
 
 export default Footer;
+// Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'darkslategray', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2024 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
