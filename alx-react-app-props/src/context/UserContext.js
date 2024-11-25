ee8808e9c512bd8a021c2fe6d21f397c6bbfa520
MaxiimeAz <graffiti2015@gmail.com>
2024-11-25 18:20:21 +0000

    Create UserContext.js

---
+++ alx-react-app-props/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+// Create the context for user data
+const UserContext = createContext();
+
+// Export the context so it can be used in other components
+export default UserContext;
