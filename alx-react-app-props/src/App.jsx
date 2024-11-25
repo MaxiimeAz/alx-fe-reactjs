0e57591669a02630bc7400ca79108dcb233cceac
MaxiimeAz <graffiti2015@gmail.com>
2024-11-25 17:12:25 +0000

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -60,7 +60,21 @@ function App() {
         </div>
     );
 }
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
 export default App;
 export default App;
 export default App;
