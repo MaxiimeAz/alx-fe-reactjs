f2fe639027df1a92e09eb055857a48b2a3f8b1b7
MaxiimeAz <graffiti2015@gmail.com>
2024-11-26 13:28:03 +0000

    Delete alx-react-app-props/src/context directory

--- alx-react-app-props/src/context/UserContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-// Create the context for user data
-const UserContext = createContext();
-
-// Export the context so it can be used in other components
-export default UserContext;
