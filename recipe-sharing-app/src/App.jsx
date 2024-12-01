7847e91284eb9c0ea30b5d30bf1ada14c890cfa9
MaxiimeAz <graffiti2015@gmail.com>
2024-12-01 14:43:16 +0000

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <Routes>
-        <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <Routes>
+          <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
