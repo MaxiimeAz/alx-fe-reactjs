e03e568c490d858f19d5d8dd8620e02352b19682
MaxiimeAz <graffiti2015@gmail.com>
2024-12-02 15:12:20 +0000

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useRecipeStore from '../store/recipeStore';
 
 const RecipeList = () => {
@@ -7,10 +8,14 @@ const RecipeList = () => {
   return (
     <ul>
       {filteredRecipes.map((recipe) => (
-        <li key={recipe.id}>
+        <li key={recipe.id} style={{ marginBottom: '16px' }}>
           <h3>{recipe.title}</h3>
           <p>Ingredients: {recipe.ingredients.join(", ")}</p>
           <p>Preparation Time: {recipe.time} mins</p>
+          {/* Link to a detailed recipe view */}
+          <Link to={`/recipe/${recipe.id}`} style={{ color: 'blue', textDecoration: 'underline' }}>
+            View Details
+          </Link>
         </li>
       ))}
     </ul>
