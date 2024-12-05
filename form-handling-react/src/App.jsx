ed588785a08a2be9ffc2c3298a1af1f9aa882b19
MaxiimeAz <graffiti2015@gmail.com>
2024-12-05 17:01:44 +0000

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,22 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
-import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/FormikForm';
 
-function App() {
-  return (
-    <div>
-      <h1>Registration Form (Controlled Components)</h1>
-      <RegistrationForm />
-      <h1>Registration Form (Formik)</h1>
-      <FormikForm />
-    </div>
-  );
-}
-
-export default App;
 function App() {
   const [count, setCount] = useState(0)
 
