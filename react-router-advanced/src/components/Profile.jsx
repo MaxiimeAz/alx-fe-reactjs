50787be358770160f2789096ba9e8600f7cae9cd
MaxiimeAz <graffiti2015@gmail.com>
2024-12-09 23:53:09 +0000

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom'; // Import Link and Outlet from react-router-dom
+import { Link, Outlet } from 'react-router-dom'; // Import Link and Outlet
 
 function Profile() {
   return (
@@ -6,15 +6,17 @@ function Profile() {
       <h2>Profile Page</h2>
       <nav>
         <ul>
+          {/* Link to the Profile Details page */}
           <li>
-            <Link to="details">Profile Details</Link> {/* Link to ProfileDetails */}
+            <Link to="details">Profile Details</Link>
           </li>
+          {/* Link to the Profile Settings page */}
           <li>
-            <Link to="settings">Profile Settings</Link> {/* Link to ProfileSettings */}
+            <Link to="settings">Profile Settings</Link>
           </li>
         </ul>
       </nav>
-      {/* Outlet renders the nested route components */}
+      {/* Outlet renders the nested route components here */}
       <Outlet />
     </div>
   );
