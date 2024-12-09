626b048af70dff6333b0366ea68d3893d1f15914
MaxiimeAz <graffiti2015@gmail.com>
2024-12-09 22:30:44 +0000

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,7 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './PostsComponent';
 
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
 function App() {
   const [count, setCount] = useState(0)
 
