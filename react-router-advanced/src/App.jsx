cd8c65e317c042ad0f0d67cd638b3c728fc03482
MaxiimeAz <graffiti2015@gmail.com>
2024-12-09 23:38:45 +0000

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile'; // Import Profile component
-import ProfileDetails from './components/ProfileDetails'; // Import ProfileDetails
-import ProfileSettings from './components/ProfileSettings'; // Import ProfileSettings
+import ProfileDetails from './components/ProfileDetails'; // Import ProfileDetails component
+import ProfileSettings from './components/ProfileSettings'; // Import ProfileSettings component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> {/* Route for home page */}
+        <Route path="/" element={<Home />} /> {/* Route for the home page */}
         <Route path="/profile" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} /> {/* Nested route for details */}
-          <Route path="settings" element={<ProfileSettings />} /> {/* Nested route for settings */}
+          <Route path="details" element={<ProfileDetails />} /> {/* Nested route for ProfileDetails */}
+          <Route path="settings" element={<ProfileSettings />} /> {/* Nested route for ProfileSettings */}
         </Route>
       </Routes>
     </Router>
@@ -22,26 +18,7 @@ function App() {
 }
 
 function Home() {
-  return <div>Welcome to the Home Page!</div>;  {/* Home page */}
-}
-
-export default App;
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-}
-
-function Home() {
-  return <div>Welcome to the Home Page!</div>;
+  return <div>Welcome to the Home Page!</div>;  {/* Home page content */}
 }
 
 export default App;
