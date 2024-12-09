e50ad3a64b009ef5b2ec85403ca8e98b82ce791f
MaxiimeAz <graffiti2015@gmail.com>
2024-12-09 23:56:35 +0000

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,23 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
+import UserProfile from './components/UserProfile';  // New dynamic route component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} /> {/* Home route */}
+        
+        {/* Profile route with nested routes */}
         <Route path="/profile" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} /> {/* Nested route for ProfileDetails */}
-          <Route path="settings" element={<ProfileSettings />} /> {/* Nested route for ProfileSettings */}
+          <Route path="details" element={<ProfileDetails />} /> {/* Nested route for details */}
+          <Route path="settings" element={<ProfileSettings />} /> {/* Nested route for settings */}
         </Route>
+        
+        {/* Dynamic route for user profile */}
+        <Route path="/user/:userId" element={<UserProfile />} /> {/* Dynamic route */}
       </Routes>
     </Router>
   );
 }
 
 function Home() {
-  return <div>Welcome to the Home Page!</div>;  {/* Home page */}
+  return <div>Welcome to the Home Page!</div>;
 }
 
 export default App;
