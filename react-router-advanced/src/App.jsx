7b66e7e803fb567943220f100e16a33b2bdc99ae
MaxiimeAz <graffiti2015@gmail.com>
2024-12-09 23:32:15 +0000

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />}>
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+function Home() {
+  return <div>Welcome to the Home Page!</div>;
+}
+
+export default App;
