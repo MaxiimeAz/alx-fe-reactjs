9546064d44cf14ca512fa9b66491f5f6bf714a41
MaxiimeAz <graffiti2015@gmail.com>
2024-12-09 15:10:38 +0000

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from "react";
-import FormikForm from "./components/formikForm";  // Should automatically resolve to formikForm.js
+import React from "react";
+import FormikForm from "./components/formikForm"; // No file extension needed
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Registration</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
 import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
 
