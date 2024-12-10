daa189373c1be0bc5b4b07b2a0ddb5b4e60f963c
MaxiimeAz <graffiti2015@gmail.com>
2024-12-10 14:42:00 +0000

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import BlogPostPage from './pages/BlogPostPage';
 import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
-import BlogPost from './pages/BlogPost'; // Import BlogPost component
+import ProtectedRoute from './components/ProtectedRoute'; // Import ProtectedRoute
 
 const App = () => {
-  const isAuthenticated = true; // Simulate authentication
-
   return (
     <BrowserRouter>
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/blog/:id" element={<BlogPostPage />} /> {/* Dynamic Routing for Blog */}
+
         {/* Protected Routes */}
         <Route
           path="/profile/*"
-          element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
         >
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
-
-        {/* Dynamic Route for Blog Posts */}
-        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
