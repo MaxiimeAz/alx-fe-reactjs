99f839d63839d3411bcfd8823dda15d801ae33a5
MaxiimeAz <graffiti2015@gmail.com>
2024-12-19 16:47:21 +0000

    Create TodoList.test.js

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,35 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import TodoList from '../TodoList';
+
+describe('TodoList Component', () => {
+  test('renders correctly', () => {
+    render(<TodoList />);
+    expect(screen.getByText('Learn React')).toBeInTheDocument();
+    expect(screen.getByText('Build a Todo List')).toBeInTheDocument();
+  });
+
+  test('can add a new todo', () => {
+    render(<TodoList />);
+    fireEvent.change(screen.getByPlaceholderText('Add a new todo'), {
+      target: { value: 'New Todo' },
+    });
+    fireEvent.submit(screen.getByText('Add Todo'));
+    expect(screen.getByText('New Todo')).toBeInTheDocument();
+  });
+
+  test('can toggle a todo between completed and not completed', () => {
+    render(<TodoList />);
+    const todo = screen.getByText('Learn React');
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle('text-decoration: line-through');
+    fireEvent.click(todo);
+    expect(todo).not.toHaveStyle('text-decoration: line-through');
+  });
+
+  test('can delete a todo', () => {
+    render(<TodoList />);
+    const deleteButton = screen.getAllByText('Delete')[0];
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+  });
+});
